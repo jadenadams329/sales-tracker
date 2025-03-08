3b5ef2383ce78e246352ae04c3f65e98850080b4
Jaden <jadenadams329@gmail.com>
2025-03-07 16:49:40 -0800

    log in endpoint done

---
+++ backend/routes/api/session.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { Op } = require("sequelize");
+const bcrypt = require("bcryptjs");
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { User } = require("../../db/models");
+const router = express.Router();
+
+//log in
+router.post("/", async (req, res, next) => {
+	const { email, password } = req.body;
+
+	const user = await User.unscoped().findOne({
+		where: {
+			email,
+		},
+	});
+
+	if (!user || !bcrypt.compareSync(password, user.password.toString())) {
+		const err = new Error("Login failed");
+		err.status = 401;
+		err.title = "Login failed";
+		err.errors = { credential: "The provided credentials were invalid." };
+		return next(err);
+	}
+
+	const safeUser = {
+		id: user.id,
+		email: user.email,
+		firstName: user.firstName,
+		lastName: user.lastName,
+		role: user.role,
+	};
+
+	await setTokenCookie(res, safeUser);
+
+	return res.json({
+		user: safeUser,
+	});
+});
+
+module.exports = router;
