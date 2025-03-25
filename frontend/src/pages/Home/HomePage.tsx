d46cb3caf9a4e8c227d36e9a064a30591fe9d42c
Jaden <jadenadams329@gmail.com>
2025-03-25 05:36:27 -0700

    removed tailwind and shadcn

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,6 +1,4 @@
-import SalesTable from "../../components/SalesTable/SalesTable";
 import { useSaleStore } from "../../store/SaleStore";
-import { salesColumns } from "../../components/SalesTable/Columns";
 import { useEffect } from "react";
 
 function HomePage() {
@@ -11,7 +9,6 @@ function HomePage() {
   return (
     <div>
       <h1>Home</h1>
-      <SalesTable columns={salesColumns} data={sales} />
     </div>
   )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,12 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],  
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:8050'
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
