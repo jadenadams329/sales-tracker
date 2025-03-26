554bd07fa53465307968cf1e0a17c19389a01eca
Jaden <jadenadams329@gmail.com>
2025-03-26 08:28:44 -0700

    sales table mvp done

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,14 +1,7 @@
 
-import { useSaleStore } from "../../store/SaleStore";
-import { useEffect } from "react";
 import SalesTrackerTable from "../../components/SalesTrackerTable/SalesTrackerTable";
 
 function HomePage() {
-  // const sales = useSaleStore((state) => state.sales);
-  // useEffect(() => {
-  //   useSaleStore.getState().setSales();
-  // }, []);
-
   return (
     <div>
       <h1>Home</h1>
